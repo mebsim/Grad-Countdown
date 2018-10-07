fa9774d3e126fdf38a418eeef13adb26f9795d92
mebsim <mebsim@users.noreply.github.com>
2018-10-07 17:35:53 -0400

    Initial Commit

---
+++ GradCountdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradCountdown
+//
+//  Created by Mohamed Ebsim on 2018-10-07.
+//  Copyright © 2018 Mohamed Ebsim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
